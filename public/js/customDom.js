fc803925c54882679c7e0f223312cf34fdb29838
Elia Narducci <Elia Narducci>
2018-06-05 07:36:17 +0200

    ddd

--- public/js/customDom.js
+++ public/js/customDom.js
@@ -1,7 +1,11 @@
+
 $(function(){
 
     $("#navbarToggleExternalContent .nav-link").on( "click", function() {
         document.getElementById("navbarToggleExternalContent").classList.remove("show");
         document.getElementById("btnMenuHome").setAttribute("aria-expanded", false);
     });
+
+  
 });
+
