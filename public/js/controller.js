75fc3d0355c4b56afa381474ec51c16709d0caec
elia9090 <elianarducciweb@gmail.com>
2018-06-09 16:42:14 +0200

    d

--- public/js/controller.js
+++ public/js/controller.js
@@ -126,6 +126,35 @@ app.controller('addUser', function ( $scope, $http, $location,$route) {
     };
 });
 
+
+
+app.controller('usersList', function ( $scope, $http, $location,$route) {
+   
+    $scope.user = JSON.parse(sessionStorage.user);
+   
+    if(!$scope.user.TYPE == "ADMIN"){
+        $location.path('/dashboard');
+    }
+
+    $http.defaults.headers.common['Authorization'] = 'Bearer ' +  $scope.user.TOKEN;
+    
+    $scope.usersList = [];
+    
+    $http.get('/listaUtenti').then((result) => {
+        
+    $scope.usersList = result.data.utenti;
+    
+    }).catch((err) => {
+        alert("Impossibile reperire la lista degli utenti");
+    });
+  
+
+    $scope.modifyUser = function (id) {
+        console.log(id);
+    };
+});
+
+
 app.controller('logoutCtrl', function ( $scope, $http, $location, $window) {
     document.getElementById("navbarToggleExternalContent").classList.remove("show");
     document.getElementById("btnMenuHome").setAttribute("aria-expanded", false);
