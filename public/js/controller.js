84f52e21ffc09d39de6e11659c285d12cfb79af3
elia9090 <elianarducciweb@gmail.com>
2018-06-10 11:16:13 +0200

    ff

--- public/js/controller.js
+++ public/js/controller.js
@@ -26,11 +26,16 @@ app.controller('loginAppCtrl', function ($scope, $http, $location, $window) {
     };
 });
 app.controller('dashboardAppCtrl', function ( $scope, $http, $location, $window) {
-
+    $scope.height_chart = "";
+    if(innerWidth<1000){
+        $scope.height_chart = 500;
+    }
+   
+    
     $scope.user = JSON.parse(sessionStorage.user);
     $scope.labels = ["OK", "KO", "In attesa"];
     $scope.data = [70, 20, 10];
-    $scope.colors = ["rgb(159,204,0)","rgb(250,109,33)","rgb(154,154,154)"];
+    $scope.colors = ["rgb(45,204,112)","rgb(232,76,61)","rgb(154,154,154)"];
     $scope.options = {
         segmentShowStroke : true,
         animationSteps : 1,
@@ -38,13 +43,14 @@ app.controller('dashboardAppCtrl', function ( $scope, $http, $location, $window)
         legend: {display: true}
     };  
     $scope.labels1 = ["Emanuele", "Nicola", "Umberto"];
-    $scope.data1 = [70, 20, 10];
-    $scope.colors1 = ["rgb(159,204,0)","rgb(250,109,33)","rgb(154,154,154)"];
+    $scope.data1 = [50, 40, 10];
+    $scope.colors1 = ["rgb(45,204,112)","rgb(232,76,61)","rgb(154,154,154)"];
     $scope.options1 = {
         segmentShowStroke : true,
         animationSteps : 1,
         animationEasing : "linear",
-        legend: {display: true}
+        legend: {display: true},
+      
     };  
 
 });
