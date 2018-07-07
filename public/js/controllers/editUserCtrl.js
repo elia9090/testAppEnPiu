ef57b39c3da7f91bc2aac3954fde5e73f3fa1125
elia9090 <elianarducciweb@gmail.com>
2018-07-07 14:58:55 +0200

    dfd

--- public/js/controllers/editUserCtrl.js
+++ public/js/controllers/editUserCtrl.js
@@ -6,19 +6,20 @@ app.controller('editUserCtrl', function ( $scope, $http, $location,$route,$route
         $location.path('/dashboard');
     }
 
+    $scope.editUser = {};
+
     $http.defaults.headers.common['Authorization'] = 'Bearer ' +  $scope.user.TOKEN;
     var id = $routeParams.id;
     $http.get('/edituser/'+id).then((result) => {
-        $scope.nome =result.data.utente.NOME;
-        $scope.cognome =result.data.utente.COGNOME;
-        $scope.username =result.data.utente.USERNAME;
-        $scope.password = "";
+        $scope.editUser.nome =result.data.utente.NOME;
+        $scope.editUser.cognome =result.data.utente.COGNOME;
+        $scope.editUser.username =result.data.utente.USERNAME;
+        $scope.editUser.password = "";
         //preseleziono lo usertype dai radio button
-        $scope.userType = result.data.utente.TIPO;
-        $scope.operatoreAssociato = result.data.utente.ID_OPERATORE;
-        $scope.responsabileAssociato = result.data.utente.ID_RESPONSABILE;
-        $scope.operatori = "";
-        $scope.responsabili = "";
+        $scope.editUser.userType = result.data.utente.TIPO;
+        $scope.editUser.operatoreAssociato = result.data.utente.ID_OPERATORE;
+        $scope.editUser.responsabileAssociato = result.data.utente.ID_RESPONSABILE;
+
         }).catch((err) => {
             alert("Impossibile reperire l'utente");
         });
@@ -28,25 +29,25 @@ app.controller('editUserCtrl', function ( $scope, $http, $location,$route,$route
 
     $http.get('/listaResponsabiliAgentiWS').then((result) => {
         
-    $scope.responsabili = result.data.responsabili;
+    $scope.editUser.responsabili = result.data.responsabili;
     }).catch((err) => {
         alert("Impossibile reperire la lista dei responsabili");
     });
     $http.get('/listaOperatoriWS').then((result) => {
-        $scope.operatori =  result.data.operatori;
+        $scope.editUser.operatori =  result.data.operatori;
     }).catch((err) => {
         alert("Impossibile reperire la lista degli operatori");
     });
 
-    $scope.submitAddUser = function () {
+    $scope.editUser.submitAddUser = function () {
         $http.post('/addUser', {
-            'username' : $scope.username,
-            'password' : $scope.password,
-            'nome': $scope.nome,
-            'cognome':$scope.cognome,
+            'username' : $scope.editUser.username,
+            'password' : $scope.editUser.password,
+            'nome': $scope.editUser.nome,
+            'cognome':$scope.editUser.cognome,
             'userType': $scope.userType,
-            'operatoreAssociato' : $scope.operatoreAssociato,
-            'responsabileAssociato' : $scope.responsabileAssociato
+            'operatoreAssociato' : $scope.editUser.operatoreAssociato,
+            'responsabileAssociato' : $scope.editUser.responsabileAssociato
         }).then((result) => {
             alert('Utente creato correttamente');
             $route.reload();
@@ -55,7 +56,7 @@ app.controller('editUserCtrl', function ( $scope, $http, $location,$route,$route
                 alert("Errore nella registrazione utente");
             }
             if(err.status === 400){
-                alert("Utente già presente: "+ $scope.username);
+                alert("Utente già presente: "+ $$scope.editUser.username);
             }
             if(err.status === 403){
                 alert("Utente non autorizzato");
