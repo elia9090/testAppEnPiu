96fef75045b0a594332305803c0efb041e919a60
elia9090 <elianarducciweb@gmail.com>
2018-11-15 11:02:47 +0100

    f

--- public/js/controllers/dashboardCtrl.js
+++ public/js/controllers/dashboardCtrl.js
@@ -56,6 +56,37 @@ app.controller('dashboardCtrl', function ( $scope, $http,$location,alertify) {
         });
 
     }
+    if($scope.user.TYPE == "OPERATORE"){
+        
+        $http.get('/dateStatsOperatoreDashboard/'+$scope.user.Id).then((result) => {
+        
+        $scope.dashBoard.stats = result.data.stats;
+
+
+
+        $scope.labels1 = ["OK", "KO", "VALUTA","ASSENTE","NON VISITATO", "DA ESITARE"];
+        $scope.data1 = [$scope.dashBoard.stats.OK_PERC, $scope.dashBoard.stats.KO_PERC, $scope.dashBoard.stats.VALUTA_PERC, $scope.dashBoard.stats.ASSENTE_PERC, $scope.dashBoard.stats.NON_VISITATO_PERC, $scope.dashBoard.stats.DA_ESITARE_PERC];
+        $scope.colors1 = ["rgb(45,204,112)","rgb(232,76,61)","rgb(255,207,21)","rgb(149,165,166)","rgb(189,195,199)","rgb(142,68,173)"];
+        $scope.options1 = {
+            segmentShowStroke : true,
+            animationSteps : 1,
+            animationEasing : "linear",
+            legend: {display: true},
+          
+        };
+           
+            
+        }).catch((err) => {
+            if(err.status === 403){
+                alertify.alert("Utente non autorizzato");
+                $location.path('/logout');
+                return;
+            }
+            alertify.alert("Impossibile reperire le statistiche appuntamenti");
+        });
+
+    }
+    
     if($scope.user.TYPE == "AGENTE" || $scope.user.TYPE == "RESPONSABILE_AGENTI"){
         
         $http.get('/dateStatsVenditoreDashboard/'+$scope.user.Id).then((result) => {
