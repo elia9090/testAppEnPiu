4603ab094cfbb22804d4aaf712e787052438df10
elia9090 <elianarducciweb@gmail.com>
2018-11-17 18:33:17 +0100

    fr

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('combine', function() {
    return gulp.src([ 
       './public/js/controllers/*.js']) 
     .pipe(concat('ng-app.js')) 
-    .pipe(gulp.dest('./public/js/controllers/dist')); 
+    .pipe(gulp.dest('./public/js/dist/')); 
 });
 
 gulp.task('default', ['combine']);
